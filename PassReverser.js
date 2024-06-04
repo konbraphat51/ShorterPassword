093cd51514c2919537858fd59b0ec43440298117
konbraphat51 <konbraphat@gmail.com>
2024-06-04 13:47:03 +0900

    Reverse()

--- PassReverser.js
+++ PassReverser.js
@@ -5,4 +5,30 @@ class PassReverser {
 	constructor(diffused) {
 		this.diffused = diffused
 	}
+
+	Reverse(input) {
+		let reversed = ""
+
+		let counters = {}
+		const keys = Object.keys(this.diffused)
+		for (let cnt = 0; cnt < keys.length; cnt++) {
+			counters[keys[cnt]] = 0
+		}
+
+		for (let cnt = 0; cnt < input.length; cnt++) {
+			let char = input[cnt]
+
+			//avoid error
+			if (counters[char] >= this.diffused[char].length) {
+				counters[char] = 0
+			}
+
+			let adding = this.diffused[char][counters[char]]
+			counters[char] += 1
+
+			reversed += adding
+		}
+
+		return reversed
+	}
 }
